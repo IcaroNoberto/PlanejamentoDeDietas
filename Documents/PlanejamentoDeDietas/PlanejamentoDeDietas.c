dece97020de481d7275fc713c9a5de3159d709a7
Ícaro Fernandes dos Santos Noberto <icaro.noberto1@gmail.com>
2019-09-14 20:11:00 -0300

    Adicionando as possiveis Validações para Nome e Numero
    Ainda não foi colocada no código, pois estamos buscando uma maneira mais eficiente de implementar elas

--- Documents/PlanejamentoDeDietas/PlanejamentoDeDietas.c
+++ Documents/PlanejamentoDeDietas/PlanejamentoDeDietas.c
@@ -72,4 +72,23 @@ int main (void){
   }
 return 0;
 }
-//scanf("%[[A-Z][a-z].* [A-Z][a-z].*]c", &nome);
+
+//int validacaoDeNumero(char c){
+//  if(c>="0"&& c<="9"){
+//    return 1;
+//  }
+//  else
+//    return 0;
+//}
+
+//int validacaoDeNome(char c) {
+//  if (c>="A" && c<="Z"){
+//    return 1;
+//  }
+//  else if (c>="a" && c<="z"){
+//    return 1;
+//  }
+//  else{
+//    return 0;
+//  }
+//}
